f9f9f96f537f9cf53d405de20ef2a3a255418ca1
DianaBerte <dianaberte.go@gmail.com>
2023-01-20 13:23:13 +0100

    got the id

---
+++ backoffice.js
@@ -0,0 +1,16 @@
+//GET https://striveschool-api.herokuapp.com/api/movies --> Array of genres (NOT MOVIES)
+//GET https://striveschool-api.herokuapp.com/api/movies/[your_genre] --> Array of movies with your genre.
+//POST: add movie from backoffice: https://striveschool-api.herokuapp.com/api/movies -->
+            // {
+            //     "_id": "63ca855b17ea7800152ebfbc",
+            //     "name": "EpiHorror",
+            //     "description": "Horror movie about coding 10 hours per day",
+            //     "category": "horror",
+            //     "imageUrl": "https://bit.ly/3cMc2IH",
+            //     "userId": "63c932b7e737380015374374",
+            //     "createdAt": "2023-01-20T12:13:15.924Z",
+            //     "updatedAt": "2023-01-20T12:13:15.924Z",
+            //     "__v": 0
+            // }
+//PUT: update existing: https://striveschool-api.herokuapp.com/api/movies/[movie_id]
+//DELETE the movies that match the movie_id passed in the url: https://striveschool-api.herokuapp.com/api/movies/[movie_id]
